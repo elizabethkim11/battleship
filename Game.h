ebcb7701b2cd754be6806a85d5ce6f43b892bb58
elizabethkim11 <105129075+elizabethkim11@users.noreply.github.com>
2022-05-18 19:49:47 -0700

    Update Game.h

--- Game.h
+++ Game.h
@@ -8,30 +8,34 @@
 #ifndef GAME_INCLUDED
 #define GAME_INCLUDED
 
-#include <iostream>
+#include <string>
+#include <cassert>
 
-using namespace std;
-
-class Player;
 class Point;
+class Player;
+class GameImpl;
 
 class Game
 {
-public:
+  public:
     Game(int nRows, int nCols);
     ~Game();
     int rows() const;
     int cols() const;
     bool isValid(Point p) const;
     Point randomPoint() const;
-    bool addShip(int length, char symbol, string name);
+    bool addShip(int length, char symbol, std::string name);
     int nShips() const;
     int shipLength(int shipId) const;
     char shipSymbol(int shipId) const;
-    string shipName(int shipId) const;
+    std::string shipName(int shipId) const;
     Player* play(Player* p1, Player* p2, bool shouldPause = true);
-private:
+      // We prevent a Game object from being copied or assigned
+    Game(const Game&) = delete;
+    Game& operator=(const Game&) = delete;
+
+  private:
     GameImpl* m_impl;
 };
 
-#endif /* GAME_INCLUDED */
+#endif // GAME_INCLUDED
