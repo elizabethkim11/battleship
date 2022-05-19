cba536570eb1eebdb4a45619a75b4a48456c8f9c
elizabethkim11 <105129075+elizabethkim11@users.noreply.github.com>
2022-05-18 19:50:13 -0700

    Update Player.h

--- Player.h
+++ Player.h
@@ -8,28 +8,40 @@
 #ifndef PLAYER_INCLUDED
 #define PLAYER_INCLUDED
 
-#include <iostream>
+#include <string>
 
+class Point;
 class Board;
 class Game;
-class Point;
-
-using namespace std;
 
 class Player
 {
-public:
-    Player(string nm, const Game& g);
-    virtual ~Player();
-    string name() const;
-    const Game& game() const;
-    virtual bool isHuman() const;
+  public:
+    Player(std::string nm, const Game& g)
+     : m_name(nm), m_game(g)
+    {}
+
+    virtual ~Player() {}
+
+    std::string name() const { return m_name; }
+    const Game& game() const { return m_game; }
+
+    virtual bool isHuman() const { return false; }
+
     virtual bool placeShips(Board& b) = 0;
     virtual Point recommendAttack() = 0;
-    virtual void recordAttackResult(Point p, bool validShot, bool shotHit, bool shipDestroyed, int shipId) = 0;
+    virtual void recordAttackResult(Point p, bool validShot, bool shotHit,
+                                        bool shipDestroyed, int shipId) = 0;
     virtual void recordAttackByOpponent(Point p) = 0;
+      // We prevent any kind of Player object from being copied or assigned
+    Player(const Player&) = delete;
+    Player& operator=(const Player&) = delete;
+
+  private:
+    std::string m_name;
+    const Game& m_game;
 };
 
-Player* createPlayer(string type, string nm, const Game& g);
+Player* createPlayer(std::string type, std::string nm, const Game& g);
 
-#endif /* PLAYER_INCLUDED */
+#endif // PLAYER_INCLUDED
